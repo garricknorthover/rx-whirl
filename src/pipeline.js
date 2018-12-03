a057b0833027ab7712cc7c76264fc1a9b515f28c
garricknorthover <garricknorthover@gmail.com>
2018-12-04 07:56:17 +1300

    int

---
+++ src/pipeline.js
@@ -0,0 +1,26 @@
+import { interval, pipe } from "rxjs";
+import { map, take } from "rxjs/operators";
+
+import { TICK, LENGTH, gaussian } from "./base";
+
+/**
+ * Build custom and reusable operators
+ * from other pipeable operators
+ * with the pipe utility function
+ */
+const draw = brush =>
+  pipe(
+    map(num => brush.repeat(Math.floor(num * 65))),
+    take(LENGTH)
+  );
+
+/**
+ * Compose operators with the 
+ * pipe method built into Observable
+ */
+const stream = interval(TICK).pipe(
+  map(gaussian),
+  draw("•")
+);
+
+stream.subscribe(console.log);
